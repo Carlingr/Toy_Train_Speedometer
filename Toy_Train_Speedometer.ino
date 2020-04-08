70898232ea8571dddf55355774f104cbb794862b
Carlin <kittyclubco@gmail.com>
2020-04-08 11:42:33 -0400

    Update Toy_Train_Speedometer.ino

--- Toy_Train_Speedometer.ino
+++ Toy_Train_Speedometer.ino
@@ -35,7 +35,7 @@ byte displays[] = {DISP_1, DISP_2, DISP_3, DISP_4}; //array used to select speci
 const int SPACING = 67; // Space between photoresistors (in mm)
 const int CALIBRATION_SAMPLES = 20;// Number of samples to take for calibration
 const unsigned long TIMEOUT = 500000; //Amount of time (microseconds) to wait for next sensor
-const unsigned int SHOW_SPEED_TIME = 500000; // Amount of time (microseconds) to show speed before resetting
+const unsigned long SHOW_SPEED_TIME = 500000; // Amount of time (microseconds) to show speed before resetting
 
 // Global variables
 int threshold_1;
@@ -46,6 +46,8 @@ int dispVal[] = {
 };
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println(analogRead(PHOTO_PIN_1));
   //next 3 lines get the shift register all set up
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
@@ -62,6 +64,7 @@ void setup() {
 }
 
 void loop() {
+  //NB: this code expects pull *up* resistors. connect the resistor to the + rail
   int photo_light_1;
   int photo_light_2;
   unsigned long time_1;
@@ -70,7 +73,7 @@ void loop() {
   photo_light_1 = analogRead(PHOTO_PIN_1);
 
   // See if object passed in front of sensor 1
-  if ( photo_light_1 <= threshold_1 ) {
+  if ( photo_light_1 >= threshold_1 ) {
 
     // Save the time at which the object was seen
     time_1 = micros();
@@ -80,7 +83,7 @@ void loop() {
 
       // If sensor 2 sees it, show speed as m/s
       photo_light_2 = analogRead(PHOTO_PIN_2);
-      if ( photo_light_2 <= threshold_2 ) {
+      if ( photo_light_2 >= threshold_2 ) {
         showSpeed(time_1);
         break;
       }
@@ -92,7 +95,6 @@ void loop() {
       }
     }
   }
-
 }
 
 void showSpeed(unsigned long t1) {
@@ -103,7 +105,6 @@ void showSpeed(unsigned long t1) {
   // Take the current time and calculate the object's speed
   t2 = micros();
   spd = (SPACING * 1000.0) / (t2 - t1);
-
   grabplace(carspeed); //break down the number into individual places
   while (t2 - micros() <= SHOW_SPEED_TIME) { //show the numbers for more than one processor cycle
     //next 3 lines: now that we have the places, send out the data
@@ -173,7 +174,7 @@ void calibrateSensors() {
 
   // Turn off LEDs and wait a few ms for them to be fully off
   digitalWrite(LEDS_PIN, LOW);
-  delay(100);
+  delay(500);
 
   // Find average light value for sensors with LEDs off
   for ( i = 0; i < CALIBRATION_SAMPLES; i++ ) {
@@ -186,7 +187,6 @@ void calibrateSensors() {
   // Calculate thresholds (half way between lit and unlit values)
   threshold_1 = ((avg_lit_1 - avg_unlit_1) / 2) + avg_unlit_1;
   threshold_2 = ((avg_lit_2 - avg_unlit_2) / 2) + avg_unlit_2;
-
   // Turn on output LEDs and wait for them to be fully on
   digitalWrite(LEDS_PIN, HIGH);
   delay(100);
